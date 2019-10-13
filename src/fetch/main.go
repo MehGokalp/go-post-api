b51dc8be215e28de5c5c6f5aba0071296573f5bd
Mehmet Gökalp <mehmet.gokalp@enuygun.com>
2019-10-13 14:14:37 +0300

    Go max proc increased

--- src/fetch/main.go
+++ src/fetch/main.go
@@ -6,6 +6,7 @@ import (
 	"post-api/pkg/fetch"
 	"post-api/pkg/post"
 	"post-api/pkg/user"
+	"runtime"
 	"strconv"
 	"sync"
 )
@@ -15,8 +16,9 @@ var userService = fetch.UserFetcher{}
 var wg = sync.WaitGroup{}
 
 func main() {
+	runtime.GOMAXPROCS(4)
 	userIds := os.Args[1:]
-	
+
 	for _, id := range userIds {
 		wg.Add(1)
 		//To run asynchronously
@@ -24,11 +26,11 @@ func main() {
 		id, _ := strconv.Atoi(id)
 		go func() {
 			fetchedPost := fetchPost(id)
-			
+
 			fmt.Println(fetchedPost)
 		}()
 	}
-	
+
 	wg.Wait()
 	fmt.Println("Done")
 }
@@ -36,26 +38,26 @@ func main() {
 func fetchPost(userId int) []post.Post {
 	defer wg.Done()
 	postData, err := postService.Fetch(userId)
-	
+
 	if err != nil {
 		panic(err.Error())
 	}
-	
+
 	fetchedUser := fetchUser(userId)
-	
+
 	for i := range postData {
 		postData[i].User = &fetchedUser
 	}
-	
+
 	return postData
 }
 
 func fetchUser(id int) user.User {
 	fetched, err := userService.Fetch(id)
-	
+
 	if err != nil {
 		panic(err.Error())
 	}
-	
+
 	return fetched
 }
